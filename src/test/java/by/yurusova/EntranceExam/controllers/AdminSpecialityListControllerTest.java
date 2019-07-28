7a821d861d34dfbc47929d680a5ef0d94fc97f4c
Yulia Yurusova <kukuzura@gmail.com>
2019-07-28 22:23:05 +0300

    Tests for AdminSpecialityListController

---
+++ src/test/java/by/yurusova/EntranceExam/controllers/AdminSpecialityListControllerTest.java
@@ -0,0 +1,89 @@
+package by.yurusova.EntranceExam.controllers;
+
+import by.yurusova.entranceExam.controllers.AdminExamListController;
+import by.yurusova.entranceExam.controllers.AdminSpecialityListController;
+import by.yurusova.entranceExam.entities.Exam;
+import by.yurusova.entranceExam.entities.Speciality;
+import by.yurusova.entranceExam.services.interfaces.ExamService;
+import by.yurusova.entranceExam.services.interfaces.SpecialityService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:test.xml", "classpath:test-database.xml"})
+@Transactional
+public class AdminSpecialityListControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AdminSpecialityListController controller;
+
+    @Autowired
+    private SpecialityService specialityService;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testGet() {
+        ResultMatcher ok = MockMvcResultMatchers.status()
+                .isOk();
+        ResultMatcher view = MockMvcResultMatchers.view()
+                .name("/adminSpecialityList.jsp");
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("http://localhost:9090/admin/specialityList");
+        try {
+            this.mockMvc.perform(builder)
+                    .andExpect(ok)
+                    .andExpect(view);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void beforeDelete() {
+        Speciality speciality = new Speciality();
+        speciality.setId(1);
+        specialityService.save(speciality);
+    }
+
+    @Test
+    public void testDelete() {
+        ResultMatcher ok = MockMvcResultMatchers.status()
+                .isOk();
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("http://localhost:9090/admin/specialityDelete/1");
+        try {
+            this.mockMvc.perform(builder)
+                    .andExpect(ok);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNull(specialityService.findById(1));
+
+    }
+
+
+    public void setController(AdminSpecialityListController controller) {
+        this.controller = controller;
+    }
+
+    public void setSpecialityService(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+}
