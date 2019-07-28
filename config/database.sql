e241a7bb8fd45d8f1382f7390d1d687a5ce26642
Yulia Yurusova <kukuzura@gmail.com>
2019-07-28 17:03:07 +0300

    database script

--- config/database.sql
+++ config/database.sql
@@ -2,7 +2,8 @@ create table role
 (
     id   int auto_increment primary key,
     name varchar(50) not null,
-    constraint role_name_uindex unique (name)
+    constraint role_name_uindex
+        unique (name)
 );
 
 create table speciality
@@ -27,18 +28,21 @@ create table user
 
 create table student
 (
-    passport_id varchar(10)      not null,
-    first_name  varchar(30)      null,
-    last_name   varchar(30)      null,
-    patronymic  varchar(30)      null,
-    user_id     int              null,
-    id          int              not null,
-    grade       double default 0 not null,
-    constraint student_id_uindex unique (id),
-    constraint student_user_id_uindex unique (user_id),
-    constraint student_user_id_fk foreign key (user_id)
-        references user (id)
-        on update cascade on delete cascade
+    passport_id varchar(10)                                                                        not null,
+    first_name  varchar(30)                                                                        null,
+    last_name   varchar(30)                                                                        null,
+    patronymic  varchar(30)                                                                        null,
+    user_id     int                                                                                null,
+    id          int                                                                                not null,
+    grade       double                                                       default 0             not null,
+    status      enum ('NEW_STUDENT', 'PASS_EXAMS', 'ENTER', 'DID_NOT_ENTER') default 'NEW_STUDENT' null,
+    constraint student_id_uindex
+        unique (id),
+    constraint student_user_id_uindex
+        unique (user_id),
+    constraint student_user_id_fk
+        foreign key (user_id) references user (id)
+            on update cascade on delete cascade
 );
 
 alter table student
@@ -51,26 +55,27 @@ create table teacher
     last_name  varchar(30) null,
     patronymic varchar(30) null,
     user_id    int         null,
-    constraint teacher_user_id_fk foreign key (user_id)
-        references user (id)
-        on delete cascade
+    constraint teacher_user_id_fk
+        foreign key (user_id) references user (id)
+            on delete cascade
 );
 
 create table exam
 (
     id            int auto_increment primary key,
-    teacher_id    int null,
-    subject_id    int null,
-    speciality_id int null,
-    constraint exam_speciality_id_fk foreign key (speciality_id)
-        references speciality (id)
-        on update cascade on delete set null,
-    constraint exam_subject_id_fk foreign key (subject_id)
-        references subject (id)
-        on update cascade on delete set null,
-    constraint exam_teacher_id_fk foreign key (teacher_id)
-        references teacher (id)
-        on update cascade on delete set null
+    teacher_id    int                  null,
+    subject_id    int                  null,
+    speciality_id int                  null,
+    is_graded     tinyint(1) default 0 null,
+    constraint exam_speciality_id_fk
+        foreign key (speciality_id) references speciality (id)
+            on update cascade on delete set null,
+    constraint exam_subject_id_fk
+        foreign key (subject_id) references subject (id)
+            on update cascade on delete set null,
+    constraint exam_teacher_id_fk
+        foreign key (teacher_id) references teacher (id)
+            on update cascade on delete set null
 );
 
 create table grade
@@ -79,22 +84,23 @@ create table grade
     exam_id    int           null,
     grade      int default 0 null,
     id         int auto_increment primary key,
-    constraint grade_exam_id_fk foreign key (exam_id)
-        references exam (id)
-        on update cascade on delete cascade,
-    constraint grade_student_id_fk foreign key (student_id)
-        references student (id)
-        on update cascade on delete cascade
+    constraint grade_exam_id_fk
+        foreign key (exam_id) references exam (id)
+            on update cascade on delete cascade,
+    constraint grade_student_id_fk
+        foreign key (student_id) references student (id)
+            on update cascade on delete cascade
 );
 
 create table user_role
 (
     user_id int null,
     role_id int null,
-    constraint user_role_role_id_fk foreign key (role_id)
-        references role (id)
-        on update cascade on delete cascade,
-    constraint user_role_user_id_fk foreign key (user_id)
-        references user (id)
-        on update cascade on delete cascade
+    constraint user_role_role_id_fk
+        foreign key (role_id) references role (id)
+            on update cascade on delete cascade,
+    constraint user_role_user_id_fk
+        foreign key (user_id) references user (id)
+            on update cascade on delete cascade
 );
+
