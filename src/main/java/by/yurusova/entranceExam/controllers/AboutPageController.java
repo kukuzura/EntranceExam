70916fbcc735a7ba03454cd0fbe1829827af027c
Yulia Yurusova <kukuzura@gmail.com>
2019-08-22 22:07:36 +0300

    about controller comments

--- src/main/java/by/yurusova/entranceExam/controllers/AboutPageController.java
+++ src/main/java/by/yurusova/entranceExam/controllers/AboutPageController.java
@@ -2,9 +2,23 @@ package by.yurusova.entranceExam.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller for about page.
+ *
+ * @author Yuliya Yurusava <y.yurusava@sam-solurions.com>
+ * @package by.yurusova.entranceExam.controllers
+ * @link http ://sam-solutions.com/
+ * @copyright 2019 SaM
+ */
 @Controller
 public class AboutPageController {
 
+    /**
+     * Method returns about page.
+     *
+     * @return about page.
+     */
     @RequestMapping("/about")
     public String showAboutPage() {
         return "/about.jsp";
